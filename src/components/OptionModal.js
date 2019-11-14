2f1124c0f539b3936f37ac092cf2b85523d3ebad
just-atem <dorrchep@gmail.com>
2019-11-14 11:13:46 +0300

    modal styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectedOptions}
     contentLabel="Selected Option"
+    closeTimeoutMS={1000}
+    className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOptions}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="modal__button" onClick={props.handleClearSelectedOptions}>Okay</button>
     </Modal>
 );
 export default OptionModal;
